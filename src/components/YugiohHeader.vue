1bdfafd73ed258ac41515d55ae9c3c86c954d4df
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-24 16:43:28 +0200

    Header finished, changed logo with no background

--- src/components/YugiohHeader.vue
+++ src/components/YugiohHeader.vue
@@ -7,9 +7,16 @@ export default {
 </script>
 
 <template>
-    <div></div>
+    <div class="d-flex p-3">
+        <img src="/public/yu-gi-oh-sign.webp" alt="">
+        <div class="ps-3 fs-3">Yu-Gi-Oh Api</div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
     
+    img {
+        height: 40px;
+    }
+
 </style>

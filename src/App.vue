bca0ac62ba20fad54b029e96534a49cbd90b37d7
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-24 17:06:38 +0200

    Dropdown togle styled

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,18 @@
   <YugiohHeader></YugiohHeader>
 
   <main class="">
-    <div class="container"></div>
+    <div class="container">
+      <div class="dropdown py-3">
+        <a class="btn btn-light dropdown-toggle text-start" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Alien
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Predator</a></li>
+          <li><a class="dropdown-item" href="#">The Thing</a></li>
+          <li><a class="dropdown-item" href="#">Hellraiser</a></li>
+        </ul>
+      </div>
+    </div>
   </main>
 
 </template>
@@ -25,6 +36,16 @@
   main {
     background-color: #D48F38;
     height: 1000px;
+
+    .btn{
+      min-width: 120px;
+      ;
+    }
+
+    .dropdown-toggle::after {
+    margin-left: 100px;
+    }
+
   }
 
 </style>
